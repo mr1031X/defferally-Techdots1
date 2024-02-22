9a449976cdf1a64f5a705e61d79846df72316783
Muaaz Tausif <muaaztausif@gmail.com>
2024-02-22 21:02:24 +0500

    prisma setup and folder structure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,16 +2,16 @@ import LoginForm from '@/app/ui/login-form';
 import DeferAllyLogo from '../ui/deferallyLogo';
 
 export default function LoginPage() {
-    return (
-        <main className="flex items-center justify-center md:h-screen">
-            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-                <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-                    <div className="w-32 text-white md:w-36">
-                        <DeferAllyLogo />
-                    </div>
-                </div>
-                <LoginForm />
-            </div>
-        </main>
-    );
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <DeferAllyLogo />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
 }
